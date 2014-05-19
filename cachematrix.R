e14ebe61b08fd86ca81ffafe25a0dd7fc0b928cd
clickgaurav <wanigaurav11@gmail.com>
2014-05-19 18:25:36 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,92 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This Function takes in the value of an invertible Matrix, stores it and then returns the inverse of the matrix
 
-## Write a short comment describing this function
+
+
+
+## takes in the value of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+
+      m <- NULL
+
+        set <- function(y) {
+
+                x <<- y
+                m <<- NULL
+
+
+
+        } ##Setting the value of the matrix
+
+        get <- function() x ##Getting the value of the Matrix
+
+
+
+        setinverse <- function(inverse) m <<- inverse ##Set the value of inverse matrix
+
+
+
+        getinverse <- function() m ##Getting the inverse value of matrix
+
+
+
+        list(set = set, get = get,
+
+             setinverse = setinverse,
+
+             getinverse = getinverse)
+
+
 }
 
+install.packages(MASS)## Installing the package of Mass which has the function for getting the inverse of a matrix
 
-## Write a short comment describing this function
+
+
+## The following function solves and returns the inverse of the special matrix "makeCacheMatrix" 
+
+
+
+## created in the previous function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+
+    m <- x$getinverse()## Gets the inverse of data from cache of previous function
+
+
+
+        if(!is.null(m)) {
+
+                message("getting cached data")
+
+
+
+                return(m)
+        } ##Checks if the inverse has already been calculated and in case it has been calculated, returns it
+
+
+
+        data <- x$get() ## If the inverse is not calcultaed it gets the data from the previous function
+
+
+
+        m <- ginv(data, ...)## Calculates the Inverse using ginv function from the MASS package
+
+
+
+        x$setinverse(m)## Assisgns the inverse value to the set inverse function of the previous function thus 
+
+
+
+        ## recursively calling it back in the previous If loop to be printed 
+
+
+
+        m ##simply prints out inverse
 }
